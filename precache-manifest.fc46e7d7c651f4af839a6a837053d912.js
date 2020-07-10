64315e21e0443a00042e2cbd5f14ff16852614dd
gbundala <gabrielbundala@gmail.com>
2020-07-10 22:57:22 +0300

    Updates

---
+++ precache-manifest.fc46e7d7c651f4af839a6a837053d912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f6edd9eede04039f8296ef4c53a6a3",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "2011b4ed7dc436bffd5e",
+    "url": "/monsters-rolodex2/static/css/main.b3a97659.chunk.css"
+  },
+  {
+    "revision": "efdaa8f6541fe00250cb",
+    "url": "/monsters-rolodex2/static/js/2.dbc8e97e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex2/static/js/2.dbc8e97e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2011b4ed7dc436bffd5e",
+    "url": "/monsters-rolodex2/static/js/main.41830902.chunk.js"
+  },
+  {
+    "revision": "b739415c68a1817e8339",
+    "url": "/monsters-rolodex2/static/js/runtime-main.ef2f38e5.js"
+  }
+]);
